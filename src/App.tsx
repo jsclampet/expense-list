e1f8acd233e4f6534d31b4ef8a1e7d41ae54d4c2
john clampet <jsclampet@gmail.com>
2023-07-04 13:31:51 -0500

    placeholder commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import "./App.css";
 import ExpenseList from "./Components/ExpenseList/ExpenseList";
+import ExpenseFilter from "./Components/ExpenseFilter/ExpenseFilter";
 import { useState } from "react";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "entertainment" },
-    { id: 2, description: "bbb", amount: 34, category: "utilities" },
-    { id: 3, description: "ccc", amount: 22, category: "grocery" },
+    { id: 1, description: "aaa", amount: 10, category: "Entertainment" },
+    { id: 2, description: "bbb", amount: 34, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 22, category: "Groceries" },
   ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) =>
           setExpenses(expenses.filter((expense) => expense.id !== id))
         }
